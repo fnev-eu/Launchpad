fd1f3e50c86a6dbae950c677ef6a01df44e7463f
Florian "0ri" Neveu <hello@fnev.eu>
2020-09-19 01:31:10 +0200

    Prepare launchpad

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>🌍 Launchpad</title>
+</head>
+<body>
+<form method="GET" action="https://duckduckgo.com/">
+    <input type="text" autocomplete="off" name="q" value="" autocapitalize="off" autocorrect="off">
+    <input type="submit">
+</form>
+
 <?php
+//http://www.esa.int/rssfeed/Our_Activities/Space_News
+?>
+</body>
+</html>
